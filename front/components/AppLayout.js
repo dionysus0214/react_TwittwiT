6f2d6b1656cef1226056bdc0eefa37031a557420
dionysus0214 <hiba0214@naver.com>
2022-03-05 21:34:05 +0900

    반응형 그리드 사용

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
@@ -13,11 +13,24 @@ const AppLayout = ({ children }) => {
         <Menu.Item key="profile">
           <Link href="/profile"><a>Profile</a></Link>
         </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
         <Menu.Item key="signup">
           <Link href="/signup"><a>회원가입</a></Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href="https://github.com/dionysus0214" target="_blank" rel="noreferrer noopener">by Dionysus</a>
+        </Col>
+      </Row>
     </div>
   );
 };
