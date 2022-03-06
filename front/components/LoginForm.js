3b0285af31e0610a53203250aec298c3a39d46e0
dionysus0214 <hiba0214@naver.com>
2022-03-06 21:22:28 +0900

    리랜더링 방지하기 위해 인라인스타일 -> styled-component 변경

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -1,6 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
 import { Form, Input, Button } from 'antd';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
 
 const LoginForm = () => {
   const [id, setId] = useState('');
@@ -8,11 +13,13 @@ const LoginForm = () => {
 
   const onChangeId = useCallback((e) => {
     setId(e.target.value);
-  });
+  }, []);
 
   const onChangePassword = useCallback((e) => {
     setPassword(e.target.value);
-  });
+  }, []);
+
+  const style = useMemo(() => ({marginTop: 10}), []);
 
   return (
     <Form>
@@ -37,10 +44,10 @@ const LoginForm = () => {
           required
         />
       </div>
-      <div>
+      <ButtonWrapper style={style}>
         <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
         <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-      </div>
+      </ButtonWrapper>
     </Form>
   );
 };
