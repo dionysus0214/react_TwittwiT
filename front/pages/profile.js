e5771a82c4d275ff24d957911cf1392024b84633
dionysus0214 <hiba0214@naver.com>
2022-03-08 21:37:42 +0900

    프로필 페이지 및 관련 component 제작

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
 
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 const Profile = () => {
+  const followerList = [{nickname: '당근'}, {nickname: '고구마'}, {nickname: '감자'}];
+  const followingList = [{ nickname: '당근' }, { nickname: '고구마' }, { nickname: '감자' }];
+  
   return (
     <>
       <Head>
         <title>내 프로필 | TwittwiT</title>
       </Head>
-      <div>내 프로필</div>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로잉 목록" data={followerList}/>
+      </AppLayout>
     </>
   );
 }
