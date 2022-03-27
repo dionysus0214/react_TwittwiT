977c202ef6b490170884098ffcd64c1f60268eea
dionysus0214 <hiba0214@naver.com>
2022-03-27 22:18:19 +0900

    saga effect

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,13 +1,13 @@
 import { all, fork, call, take, put } from 'redux-saga/effects';
 import axios from 'axios';
 
-function logInAPI() {
-  return axios.post('/api/login');
+function logInAPI(data) {
+  return axios.post('/api/login', data);
 }
 
-function* logIn() {
+function* logIn(action) {
   try {
-    const result = yield call(logInAPI);
+    const result = yield call(logInAPI, action.data);
     yield put({
       type : 'LOG_IN_SUCCESS',
       data : result.data,
@@ -20,16 +20,54 @@ function* logIn() {
   }
 }
 
+function logOutAPI() {
+  return axios.post('/api/logout');
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutAPI);
+    yield put({
+      type : 'LOG_OUT_SUCCESS',
+      data : result.data,
+    });
+  } catch(err) {
+    yield put({ // put은 dispatch
+      type : 'LOG_OUT_FAILURE',
+      data : err.response.data,
+    });
+  }
+}
+
+function addPostAPI(data) {
+  return axios.post('/api/post', data);
+}
+
+function* addPost(action) {
+  try {
+    const result = yield call(addPostAPI, action.data);
+    yield put({
+      type : 'ADD_POST_SUCCESS',
+      data : result.data,
+    });
+  } catch(err) {
+    yield put({ // put은 dispatch
+      type : 'ADD_POST_FAILURE',
+      data : err.response.data,
+    });
+  }
+}
+
 function* watchLogIn() {
   yield take('LOG_IN_REQUEST', logIn);
 }
 
 function* watchLogOut() {
-  yield take('LOG_OUT_REQUEST');
+  yield take('LOG_OUT_REQUEST', logOut);
 }
 
 function* watchAddPost() {
-  yield take('ADD_POST_REQUEST');
+  yield take('ADD_POST_REQUEST', addPost);
 }
 
 export default function* rootSaga() {
